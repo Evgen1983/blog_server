44d50dfaf978b64b3ec294f36966b9a7f2c7f06e
Evgen1983 <ebaranov1983@gmail.com>
2017-08-04 20:02:15 +0300

    fix mistakes

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var application = express();
+
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
+var cors = require('cors');
+var posts = require('./items').posts;
+
+const port = 3001;
+
+application.use(bodyParser.urlencoded({ extended: false }));
+application.use(cors());
+application.use(express.static('public'));
+
+application.get('/', function(req, res) {
+  res.json(posts);
+});
+
+application.get('/posts/:id', function(req, res) {
+  const id = req.params.id;
+  const index = posts.findIndex((post) => post.id == id);
+
+  res.json(posts[index]);
+});
+
+application.put('/posts/:id/like', jsonParser, function(req, res) {
+  const id = req.params.id;
+  const index = posts.findIndex((post) => post.id == id);
+
+  posts[index].meta.likesCount++;
+
+  res.json(posts[index]);
+});
+
+
+
+application.listen(port, function() {
+  console.log(`Server listening on the port: ${port}`)
+});
---
+++ items.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+  posts: [
+  
+    {
+      id: 0,
+      image: {
+        src: '/images/image_1.jpg',
+        alt: 'Сакура'
+      },
+      meta: {
+        author: 'Фотограф из Японии',
+        createdAt: '2017-06-07',
+        updatedAt: '2017-06-10',
+        likesCount: 2
+      },
+      text: 'Цветение сакуры в Японии'
+    },
+
+    {
+      id: 1,
+      image: {
+        src:'/images/image_2.jpg',
+        alt: 'УАЗ'
+      },
+      meta: {
+        author: 'Фотограф 2',
+        createdAt: '2017-06-11',
+        updatedAt: '2017-06-11',
+        likesCount: 2
+      },
+      text: 'УАЗ Патриот 2016'
+    },
+
+    {
+      id: 2,
+    
+      image: {
+        src: '/images/image_3.jpg',
+        alt: 'Самолет'
+      },
+      meta: {
+        author: 'Фотограф 3',
+        createdAt: '2017-06-12',
+        updatedAt: '2017-06-12',
+        likesCount: 2
+      },
+      text: 'Первый самолет'
+    }
+  ]
+};
